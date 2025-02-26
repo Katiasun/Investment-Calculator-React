be5b6e14ce9552a38d148c005e9cd3a406719e8e
Katiasun <katjatolok3@gmail.com>
2025-02-26 18:58:41 +0100

    -

--- src/components/UserInput.jsx
+++ src/components/UserInput.jsx
@@ -32,18 +32,33 @@ export default function UserInput() {
           </p>
           <p>
             <label>Annual Investment</label>
-            <input type="number" required />
+            <input
+              type="number"
+              required
+              value={userInput.annualInvestment}
+              onChange={(event) => handleChange("annualInvestment", event.target.value)}
+            />
           </p>
         </div>
         <div className="input-group">
           <p>
             <label>Expected Return</label>
-            <input type="number" required />
+            <input
+              type="number"
+              required
+              value={userInput.expectedReturn}
+              onChange={(event) => handleChange("expectedReturn", event.target.value)}
+            />
           </p>
 
           <p>
             <label>Duration</label>
-            <input type="number" required />
+            <input
+              type="number"
+              required
+              value={userInput.duration}
+              onChange={(event) => handleChange("duration", event.target.value)}
+            />
           </p>
         </div>
       </form>
