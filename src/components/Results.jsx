2d48ca31648c861d5c034d11fde6d96fcdfdcb74
Katiasun <katjatolok3@gmail.com>
2025-02-27 15:30:38 +0100

    add some comments in file Results.jsx

--- src/components/Results.jsx
+++ src/components/Results.jsx
@@ -1,7 +1,10 @@
 import { calculateInvestmentResults, formatter } from "../util/investment.js";
 
+// Component for displaying investment results
 export default function Results({ input }) {
+  // Calculate investment results based on input data.
   const resultsData = calculateInvestmentResults(input);
+  // We determine the initial investment amount by subtracting the accrued interest and annual investment from the total cost at the end of the first year.
   const initialInvestment =
     resultsData[0].valueEndOfYear - resultsData[0].interest - resultsData[0].annualInvestment;
   console.log(resultsData);
@@ -18,9 +21,11 @@ export default function Results({ input }) {
       </thead>
       <tbody>
         {resultsData.map((yearData) => {
+          // Calculate total interest and invested capital for the current year
           const totalInterest =
             yearData.valueEndOfYear - yearData.annualInvestment * yearData.year - initialInvestment;
           const totalAmountInvested = yearData.valueEndOfYear - totalInterest;
+          // Return a table row with data for the current year
           return (
             <tr key={yearData.year}>
               <th>{yearData.year}</th>
