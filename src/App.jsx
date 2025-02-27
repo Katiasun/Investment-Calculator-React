91b28ea78a3bcc3113efd628fe4576d86882a68c
Katiasun <katjatolok3@gmail.com>
2025-02-27 15:44:58 +0100

    Added input management and validation of investment duration. Added inputIsValid check, which displays a message if the duration is incorrect.

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import "./util/investment.js";
 import { useState } from "react";
 
 function App() {
+  // Use the useState hook to store the state of user-entered data
   const [userInput, setUserInput] = useState({
     initialInvestment: 10000,
     annualInvestment: 1200,
@@ -12,10 +13,16 @@ function App() {
     duration: 10,
   });
 
+  // Check whether the entered investment duration is valid (greater than or equal to 1)
+  const inputIsValid = userInput.duration >= 1;
+
+  // Function to handle changes in input data
   function handleChange(inputIdentifier, newValue) {
+    // Update the userInput state, preserving the previous values ​​and changing only the required field
     setUserInput((prevUserInput) => {
       return {
         ...prevUserInput,
+        // Update the value of the corresponding field, bringing it to a number
         [inputIdentifier]: +newValue,
       };
     });
@@ -25,7 +32,8 @@ function App() {
     <>
       <Header />
       <UserInput onChangeInput={handleChange} userInput={userInput} />
-      <Results input={userInput} />
+      {!inputIsValid && <p className="center">Please enter a duration greater than zero.</p>}
+      {inputIsValid && <Results input={userInput} />}
     </>
   );
 }
